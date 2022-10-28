336833e40396dac5e511c84301c6cd8502c9ec2d
Lisa <cse_1912020239@lus.ac.bd>
2022-10-28 20:17:12 +0600

    SplashScreen Modified

--- app/src/main/java/com/example/cryptanalyst/SplashScreen.java
+++ app/src/main/java/com/example/cryptanalyst/SplashScreen.java
@@ -2,49 +2,49 @@ package com.example.cryptanalyst;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.annotation.Annotation;
 
 //import com.github.ybq.android.spinkit.SpinKitView;
 
 
 public class SplashScreen extends AppCompatActivity {
 
-    //private SpinKitView spinKitView;
+    private ImageView image;
+    private TextView text;
+    float v =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        //spinKitView = findViewById(R.id.progressBar);
+        image = findViewById(R.id.imageView);
+        text = findViewById(R.id.name);
 
-        //firestore = FirebaseFirestore.getInstance();
+        image.setTranslationY(-1600);
+        text.setTranslationY(1400);
+        image.setAlpha(v);
+        text.setAlpha(v);
+        image.animate().translationY(0).alpha(1).setDuration(600).setStartDelay(0).start();
 
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                doWork();
-                startApp();
-            }
-        });
-        thread.start();
-    }
+        text.animate().translationY(0).alpha(1).setDuration(600).setStartDelay(0).start();
 
-
-    public void doWork() {
-        for (int progress = 50; progress <= 100; progress += 50) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void startApp() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                finish();
+        new Handler().postDelayed(() -> {
+            image.animate().translationY(-1600).setDuration(1500).setStartDelay(100);
+            text.animate().translationY(1400).setDuration(1500).setStartDelay(100);
+        }, 1500);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }, 2200);
     }
 
 }
